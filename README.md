77d4cdea46010e4acb249d359a9d2e13ee7e605d
Roland Levy <38256994+rolandjlevy@users.noreply.github.com>
2022-04-11 21:34:13 +0000

    improved the UI

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Emoji explorer
 
-> Explore hundreds of different emojis and play them like a slide show
+> Explore hundreds of different emojis and play them like slide shows. Built with vanilla JavaScript and CSS.
 
 - [Live demo](https://js-emoji-animation.rolandjlevy.repl.co/)
 - [Source code](https://replit.com/@RolandJLevy/js-emoji-animation)
--- src/UI.js
+++ src/UI.js
@@ -6,7 +6,7 @@ export default class UI {
     return `
       <section class="emoji-section">
         <h3 id="${sectionType}-header">${section}<sup></sup></h3>
-        <input id="${sectionType}-slider" type="range" min="1" step="1" value="1" />
+        <input id="${sectionType}-slider" type="range" min="1" step="1" value="1" class="slider" />
         <div id="${sectionType}-emoji" class="emoji"> </div>
         <div id="${sectionType}-button" class='btn'>▶️</div>
       </section>`;
